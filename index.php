e55b94ea446681ddeefe0f03e6f69837ff0de3f2
Helena Edblad <Helena@Helenas-MBP.lan>
2017-10-17 23:19:59 +0200

    Added background and style

--- index.php
+++ index.php
@@ -4,20 +4,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <title>Inlämningsuppgift 2 - Helena Edblad</title>
+    
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    
+    <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
     <?php
-    
     require 'parts/database.php';
     require 'parts/fetch_todos.php';
-    
-    foreach($todos as $do_this){ 
-        
-        echo $do_this["title"]; 
-
-    }
-   
-   
     ?>
+    
+    <main>
+    
+    <?php foreach($todos as $do_this){ ?>
+        
+        <input type="checkbox" name="<?= $do_this["id"]; ?>" id="<?= $do_this["id"]; ?>">
+        <label for="<?= $do_this["id"]; ?>"><?= $do_this["title"]; ?></label>
+        <br />
+
+    <?php } ?>
+    
+    </main>
+    
+    
+    <!-- Latest compiled and minified JavaScript -->
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </body>
 </html>
