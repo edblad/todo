0989637ac971515f671b314d03e6e4a4771f353f
Helena Edblad <Helena@Helenas-MacBook-Pro.local>
2017-10-18 10:05:41 +0200

    Added button for done

--- index.php
+++ index.php
@@ -1,37 +1,42 @@
-<!DOCTYPE html>
-<html lang="sv">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <title>Inlämningsuppgift 2 - Helena Edblad</title>
-    
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-</head>
-<body>
-    <?php
-    require 'parts/database.php';
-    require 'parts/fetch_todos.php';
-    ?>
+<?php
+require 'parts/head.php';
+require 'parts/database.php';
+require 'parts/fetch_todos.php';
+?>
+
+<div class="wrapper">
+   
+    <header>
+        <h1>Att göra</h1>
+    </header>
     
     <main>
-    
-    <?php foreach($todos as $do_this){ ?>
-        
-        <input type="checkbox" name="<?= $do_this["id"]; ?>" id="<?= $do_this["id"]; ?>">
-        <label for="<?= $do_this["id"]; ?>"><?= $do_this["title"]; ?></label>
-        <br />
+    <form action="index.php" method="POST">
+        <?php foreach($todos as $do_this){ ?>
 
-    <?php } ?>
-    
-    </main>
-    
-    
-    <!-- Latest compiled and minified JavaScript -->
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-</body>
-</html>
+            <input type="checkbox" name="<?= $do_this["title"]; ?>" id="<?= $do_this["id"]; ?>">
+            <label for="<?= $do_this["id"]; ?>"><?= $do_this["title"]; ?></label>
+            <br />
+
+        <?php } ?>
+        <input type="submit" value="Avklarat" class="btn btn-success">
+    </form>
+
+    <div class="done">
+        <h2>Klart</h2>
+        <?php 
+        
+        foreach($_POST as $done){
+            echo $done;
+            echo "<br />";
+        }
+        ?>
+        
+        <?php var_dump($_POST);?>
+    </div>
+   </main>
+   
+</div>
+
+
+<?php require 'parts/foot.php'; ?>
---
+++ parts/head.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <title>Inlämningsuppgift 2 - Helena Edblad</title>
+    
+    <link href="https://fonts.googleapis.com/css?family=Gloria+Hallelujah|Roboto" rel="stylesheet">
+    
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
