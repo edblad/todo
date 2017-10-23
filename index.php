4763c87f91c9fb5ec7cfc6efe51c715a3dacda75
Helena Edblad <Helena@Helenas-MBP.lan>
2017-10-23 21:27:44 +0200

    New style

--- index.php
+++ index.php
@@ -7,69 +7,75 @@ require 'parts/add_todos.php';
 require 'parts/delete_todos.php';
 ?>
 
-<div class="wrapper">
+
    
-    <header>
-        <h1>Att-göra-lista</h1>
-    </header>
-    
-    <main>
-        <div class="add_new">
-            <h2>Lägg till ny</h2>
-               <?php
-                foreach($todos as $do_this){
-                   if(isset($_GET["id"]) == $do_this["id"]){
-                        echo "<div class='added_task_message'>Du har lagt till en ny uppgift!</div>";
-                    }
+<header>
+    <h1>Att-göra-lista</h1>
+</header>
+
+<main>
+    <div class="add_new">
+        <h2>Lägg till ny uppgift</h2>
+           <?php
+            foreach($todos as $do_this){
+               if(isset($_GET["id"]) && ($_GET["id"] == $do_this["id"])){
+                    echo "<div class='added_task_message'>Du har lagt till en ny uppgift!</div>";
                 }
+            }
+           ?>
+        <form action="parts/add_todos.php" method="POST">
+           <span class="new">
+               <label for="title">Att göra:&nbsp;</label>
+               <input type="text" name="title" id="title">
+            </span>
+           <span class="new">
+               <label for="createdBy">Skapad av:&nbsp;</label>
+               <input type="text" name="createdBy" id="createdBy">
+            </span>
+            <br />
+           <input type="submit" value="Lägg till" name="add" class="btn btn-success">
+        </form>
+    </div>
+
+    <br /><br />
+
+    <div class="todo">
+        <h2>Att göra</h2>
+        <form action="parts/delete_todos.php" method="POST">
+            <?php 
+            foreach($todos as $do_this){
+
+                if($do_this["completed"] == 0){
                ?>
-            <form action="parts/add_todos.php" method="POST">
-               <span class="new"><label for="title">Att göra:&nbsp;</label><input type="text" name="title"></span><br />
-               <label for="createdBy">Skapad av:&nbsp;</label><input type="text" name="createdBy"><br />
-               <input type="submit" value="Lägg till" name="add" class="btn btn-success">
-            </form>
-        </div>
-
-        <br /><br />
-        
-        <div class="todo">
-            <h2>Att göra</h2>
-            <form action="parts/delete_todos.php" method="POST">
-                <?php 
-                foreach($todos as $do_this){
-    
-                    if($do_this["completed"] == 0){
-                   ?>
-                        <input type="checkbox" name="<?= $do_this["id"]; ?>" id="<?= $do_this["id"]; ?>" value="<?= $do_this["id"]; ?>">
-                        <label for="<?= $do_this["id"]; ?>"><?= $do_this["title"]; ?></label>
-                        <br />
-
-                <?php }} ?>
-
-                <input type="submit" value="Klart" name="done" class="btn btn-success">
-            </form>
-       </div>
-       
-        <div class="done">
-            <h2>Klart</h2>
-
-           <form action="parts/delete_todos.php" method="POST">
-            <?php foreach($todos as $do_this){ ?>
-                
-                <?php 
-                if($do_this["completed"] == 1){ ?>
                     <input type="checkbox" name="<?= $do_this["id"]; ?>" id="<?= $do_this["id"]; ?>" value="<?= $do_this["id"]; ?>">
                     <label for="<?= $do_this["id"]; ?>"><?= $do_this["title"]; ?></label>
                     <br />
-                <?php } ?>
-                
+
+            <?php }} ?>
+
+            <input type="submit" value="Klart" name="done" class="btn btn-success">
+        </form>
+   </div>
+
+    <div class="done">
+        <h2>Klart</h2>
+
+       <form action="parts/delete_todos.php" method="POST">
+        <?php foreach($todos as $do_this){ ?>
+
+            <?php 
+            if($do_this["completed"] == 1){ ?>
+                <input type="checkbox" name="<?= $do_this["id"]; ?>" id="<?= $do_this["id"]; ?>" value="<?= $do_this["id"]; ?>">
+                <label for="<?= $do_this["id"]; ?>"><?= $do_this["title"]; ?></label>
+                <br />
             <?php } ?>
-            <input type="submit" value="Ta bort" name="delete" class="btn btn-danger">
-            </form>
-        </div>
-   </main>
-   
-</div>
+
+        <?php } ?>
+        <input type="submit" value="Ta bort" name="delete" class="btn btn-danger">
+        </form>
+    </div>
+</main>
+
 
 
 <?php require 'parts/foot.php'; ?>
--- parts/head.php
+++ parts/head.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <title>Inlämningsuppgift 2 - Helena Edblad</title>
     
-    <link href="https://fonts.googleapis.com/css?family=Gloria+Hallelujah|Roboto" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto|Josefin+Sans:400,700" rel="stylesheet">
     
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
